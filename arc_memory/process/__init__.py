240ca4700230a5e72467321891b224acd713472c
Jarrod Barnes <jbarnes850@gmail.com>
2025-05-09 07:55:33 -0400

    Implement extraction of causal relationships from commit messages, PR descriptions, Linear tickets, and ADRs

--- arc_memory/process/__init__.py
+++ arc_memory/process/__init__.py
@@ -7,9 +7,11 @@ after the initial data ingestion.
 from arc_memory.process.semantic_analysis import enhance_with_semantic_analysis
 from arc_memory.process.kgot import enhance_with_reasoning_structures
 from arc_memory.process.temporal_analysis import enhance_with_temporal_analysis
+from arc_memory.process.causal_extraction import extract_causal_relationships
 
 __all__ = [
     "enhance_with_semantic_analysis",
     "enhance_with_reasoning_structures",
     "enhance_with_temporal_analysis",
+    "extract_causal_relationships",
 ]
